2e911a7489e45a6dcf59021491638ab7305e84db
hmapodac <hmapodac@ncsu.edu>
2024-05-30 23:30:23 -0400

    Update to fix bug in hello world n

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ def hello_world():
 def hello_world_n(N):
 	'''"hello_world_n(N)" will take one argument (N) and 
 	will return the string "Hello World! Hello World! 
-	Hello World! ... Hello World!" with exactly N repetitions.'''
+	Hello World! ... Hello World!" with exactly N 
+	repetitions.'''
 	TMP = "Hello World! "
-	return TMP*N[:-1]
+	return (TMP*N)[:-1]
