b520bae73c06375cb69debe49df9768024a88472
hmapodac <hmapodac@ncsu.edu>
2024-05-30 23:25:59 -0400

    Add hello world n function

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 def hello_world():
 	return "Hello"
+
+def hello_world_n(N):
+	'''"hello_world_n(N)" will take one argument (N) and 
+	will return the string "Hello World! Hello World! 
+	Hello World! ... Hello World!" with exactly N repetitions.'''
+	TMP = "Hello World! "
+	return TMP*N[:-1]
